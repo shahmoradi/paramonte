66752db2c65896ea2fe8d437c39f5b37702e08f0
Amir Shahmoradi <shahmoradi@utexas.edu>
2024-03-24 00:04:58 -0500

    latest documentation builds.

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+####################################################################################################################################
+####################################################################################################################################
+####                                                                                                                            ####
+####    ParaMonte: Parallel Monte Carlo and Machine Learning Library.                                                           ####
+####                                                                                                                            ####
+####    Copyright (C) 2012-present, The Computational Data Science Lab                                                          ####
+####                                                                                                                            ####
+####    This file is part of the ParaMonte library.                                                                             ####
+####                                                                                                                            ####
+####    LICENSE                                                                                                                 ####
+####                                                                                                                            ####
+####       https://github.com/cdslaborg/paramonte/blob/main/LICENSE.md                                                          ####
+####                                                                                                                            ####
+####################################################################################################################################
+####################################################################################################################################
+
+source_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+cd "${source_dir}"
+
+####
+#### push the output docs to the server.
+####
+
+git add --all && git commit -m"latest documentation builds." && git push --all
