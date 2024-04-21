c16c84a428db3c79be4cbc611a4336470a8aab87
Amir Shahmoradi <shahmoradi@utexas.edu>
2024-04-20 20:33:00 -0500

    latest build.

--- fortran/latest/structpm__sampling_1_1paradram__type.js
+++ fortran/latest/structpm__sampling_1_1paradram__type.js
@@ -5,5 +5,5 @@ var structpm__sampling_1_1paradram__type =
     [ "proposalAdaptationCountGreedy", "structpm__sampling_1_1paradram__type.html#a598ab6f9c2395211bdc7c390d97bd948", null ],
     [ "proposalAdaptationPeriod", "structpm__sampling_1_1paradram__type.html#ac78fb68f583bcfec745008add915734e", null ],
     [ "proposalDelayedRejectionCount", "structpm__sampling_1_1paradram__type.html#a5e1486bad13ea471da6491ce96dae3fd", null ],
-    [ "proposalDelayedRejectionScaleFactor", "structpm__sampling_1_1paradram__type.html#ae9f37cad725948a8ff2c458b3cf27bba", null ]
+    [ "proposalDelayedRejectionScale", "structpm__sampling_1_1paradram__type.html#a29dbf38f9b8ebff62c56dafaac42828a", null ]
 ];
--- fortran/latest/structpm__sampling_1_1paramcmc__type.js
+++ fortran/latest/structpm__sampling_1_1paramcmc__type.js
@@ -4,12 +4,12 @@ var structpm__sampling_1_1paramcmc__type =
     [ "outputSampleRefinementCount", "structpm__sampling_1_1paramcmc__type.html#adb5743c90fbd68a367679c1f430a4ef7", null ],
     [ "outputSampleRefinementMethod", "structpm__sampling_1_1paramcmc__type.html#a10235dff2a17923da36d473bc539578d", null ],
     [ "proposal", "structpm__sampling_1_1paramcmc__type.html#a3051ae12161aaf6244f52d139ddfc79f", null ],
-    [ "proposalCorMat", "structpm__sampling_1_1paramcmc__type.html#abf63f8f5630b8185a80d41fd054fec96", null ],
-    [ "proposalCovMat", "structpm__sampling_1_1paramcmc__type.html#acde84dfb122a95c050e8ba14dcf1c500", null ],
-    [ "proposalScaleFactor", "structpm__sampling_1_1paramcmc__type.html#ac04daac66bc34da8bd6cff3f8134b649", null ],
+    [ "proposalCor", "structpm__sampling_1_1paramcmc__type.html#a07fb734b81f9907786a8e77b3e0998c4", null ],
+    [ "proposalCov", "structpm__sampling_1_1paramcmc__type.html#aa0e9ea3ee1d9af08191943d730ee8f40", null ],
+    [ "proposalScale", "structpm__sampling_1_1paramcmc__type.html#a086978bffde5ac9181ae6e4ff84687e6", null ],
     [ "proposalStart", "structpm__sampling_1_1paramcmc__type.html#ad82cee6e3f9912470a86dce5a1e679b2", null ],
     [ "proposalStartDomainCubeLimitLower", "structpm__sampling_1_1paramcmc__type.html#a9a7a0caf57170b16d377350cf4a32020", null ],
     [ "proposalStartDomainCubeLimitUpper", "structpm__sampling_1_1paramcmc__type.html#a69e706383ea8d8008654529b771f2e14", null ],
     [ "proposalStartRandomized", "structpm__sampling_1_1paramcmc__type.html#ae1fb3117a1629f20467fdb416ab46317", null ],
-    [ "proposalStdVec", "structpm__sampling_1_1paramcmc__type.html#ac17299da60afaa08eabcfe651ed77968", null ]
+    [ "proposalStd", "structpm__sampling_1_1paramcmc__type.html#ad3f3328c4c0897e581a5fc640690f6a2", null ]
 ];
